9ae79b0153b7c6104d6da0a573a981d7fdefaa9e
Ruizhang666 <112138222+Ruizhang666@users.noreply.github.com>
2023-03-03 13:10:07 -0500

    Add files via upload

---
+++ search_engine.py
@@ -0,0 +1,401 @@
+import itertools
+import re
+from collections import Counter, defaultdict
+from typing import Dict, List, NamedTuple
+
+import numpy as np
+from numpy.linalg import norm
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
+
+
+
+
+### File IO and processing
+
+class Document(NamedTuple):
+    doc_id: int
+    author: List[str]
+    title: List[str]
+    keyword: List[str]
+    abstract: List[str]
+
+    def sections(self):
+        return [self.author, self.title, self.keyword, self.abstract]
+
+    def __repr__(self):
+        return (f"doc_id: {self.doc_id}\n" +
+            f"  author: {self.author}\n" +
+            f"  title: {self.title}\n" +
+            f"  keyword: {self.keyword}\n" +
+            f"  abstract: {self.abstract}")
+
+
+def read_stopwords(file):
+    with open(file) as f:
+        return set([x.strip() for x in f.readlines()])
+
+stopwords = read_stopwords('common_words')
+
+stemmer = SnowballStemmer('english')
+
+def read_rels(file):
+    '''
+    Reads the file of related documents and returns a dictionary of query id -> list of related documents
+    '''
+    rels = {}
+    with open(file) as f:
+        for line in f:
+            qid, rel = line.strip().split()
+            qid = int(qid)
+            rel = int(rel)
+            if qid not in rels:
+                rels[qid] = []
+            rels[qid].append(rel)
+    return rels
+
+def read_docs(file):
+    '''
+    Reads the corpus into a list of Documents
+    '''
+    docs = [defaultdict(list)]  # empty 0 index
+    category = ''
+    with open(file) as f:
+        i = 0
+        for line in f:
+            line = line.strip()
+            if line.startswith('.I'):
+                i = int(line[3:])
+                docs.append(defaultdict(list))
+            elif re.match(r'\.\w', line):
+                category = line[1]
+            elif line != '':
+                for word in word_tokenize(line):
+                    docs[i][category].append(word.lower())
+
+    return [Document(i + 1, d['A'], d['T'], d['K'], d['W'])
+        for i, d in enumerate(docs[1:])]
+
+def stem_doc(doc: Document):
+    return Document(doc.doc_id, *[[stemmer.stem(word) for word in sec]
+        for sec in doc.sections()])
+
+def stem_docs(docs: List[Document]):
+    return [stem_doc(doc) for doc in docs]
+
+def remove_stopwords_doc(doc: Document):
+    return Document(doc.doc_id, *[[word for word in sec if word not in stopwords]
+        for sec in doc.sections()])
+
+def remove_stopwords(docs: List[Document]):
+    return [remove_stopwords_doc(doc) for doc in docs]
+
+
+
+### Term-Document Matrix
+
+class TermWeights(NamedTuple):
+    author: float
+    title: float
+    keyword: float
+    abstract: float
+
+def compute_doc_freqs(docs: List[Document]):
+    '''
+    Computes document frequency, i.e. how many documents contain a specific word
+    '''
+    freq = Counter()
+    for doc in docs:
+        words = set()
+        for sec in doc.sections():
+            for word in sec:
+                words.add(word)
+        for word in words:
+            freq[word] += 1
+    return freq
+
+def compute_tf(doc: Document, doc_freqs: Dict[str, int], weights: list):
+    vec = defaultdict(float)
+    for word in doc.author:
+        vec[word] += weights.author
+    for word in doc.keyword:
+        vec[word] += weights.keyword
+    for word in doc.title:
+        vec[word] += weights.title
+    for word in doc.abstract:
+        vec[word] += weights.abstract
+    return dict(vec)  # convert back to a regular dict
+
+def compute_tfidf(doc, doc_freqs, weights):
+
+    # convert the term counts into term frequency  
+    term_frequency=compute_tf(doc, doc_freqs, weights)
+
+    # compute the tf-idf
+
+    out=defaultdict(float)
+
+    for i in term_frequency.keys():
+        tf=term_frequency[i]
+        if doc_freqs[i]!=0:
+            idf=1/doc_freqs[i]
+        else:
+            idf=1
+        out[i]=tf*idf
+
+    return out
+
+
+def compute_boolean(doc, doc_freqs, weights):
+
+    tf=compute_tf(doc, doc_freqs, weights)
+
+    for i in tf.keys():
+        tf[i]==1
+
+    return tf
+
+
+
+### Vector Similarity
+
+def dictdot(x: Dict[str, float], y: Dict[str, float]):
+    '''
+    Computes the dot product of vectors x and y, represented as sparse dictionaries.
+    '''
+    keys = list(x.keys()) if len(x) < len(y) else list(y.keys())
+    return sum(x.get(key, 0) * y.get(key, 0) for key in keys)
+
+def cosine_sim(x, y):
+    '''
+    Computes the cosine similarity between two sparse term vectors represented as dictionaries.
+    '''
+    num = dictdot(x, y)
+    if num == 0:
+        return 0
+    return num / (norm(list(x.values())) * norm(list(y.values())))
+
+def dice_sim(x, y):
+    num=2*dictdot(x, y)
+    if num==0:
+        return 0
+    return num / (sum(list(x.values()))+sum(list(y.values())))
+
+def jaccard_sim(x, y):
+    num=dictdot(x, y)
+    if num==0:
+        return 0
+    deno=(sum(list(x.values()))+sum(list(y.values())))-num
+    if deno==0: 
+        return 0
+    return num/deno  
+
+def overlap_sim(x, y):
+
+    num=dictdot(x, y)
+    if num==0:
+        return 0
+    deno=min(sum(list(x.values())),sum(list(y.values())))
+    if deno==0: 
+        return 0
+    return num/deno  
+
+### Precision/Recall
+
+def interpolate(x1, y1, x2, y2, x):
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return m * x + b
+
+def precision_at(recall: float, results: List[int], relevant: List[int]) -> float:
+    '''
+    This function should compute the precision at the specified recall level.
+    If the recall level is in between two points, you should do a linear interpolation
+    between the two closest points. For example, if you have 4 results
+    (recall 0.25, 0.5, 0.75, and 1.0), and you need to compute recall @ 0.6, then do something like
+
+    interpolate(0.5, prec @ 0.5, 0.75, prec @ 0.75, 0.6)
+
+    Note that there is implicitly a point (recall=0, precision=1).
+
+    `results` is a sorted list of document ids
+    `relevant` is a list of relevant documents
+    '''
+    
+    # compute the recall-precision pair 
+
+    precision_list=[1]
+    recall_list=[0]
+
+    for i in range(len(relevant)):
+        num_relavant_needed=i+1
+        current_relavant=0
+        total_retrived=0
+        for j in range(len(results)):
+            current_doc=results[j]
+            if current_doc in relevant:
+                current_relavant+=1
+            total_retrived+=1
+            if current_relavant==num_relavant_needed:
+                break
+        precision=current_relavant/total_retrived
+        current_recall=num_relavant_needed/len(relevant)
+        precision_list.append(precision)
+        recall_list.append(current_recall)
+
+    for i in range(len(recall_list)):
+        if recall_list[i]==recall:
+            return precision_list[i]
+
+    for i in range(len(recall_list)):
+        if i!= len(recall_list)-1:
+            if recall_list[i]<recall and recall_list[i+1]>recall:
+                return interpolate(recall_list[i],precision_list[i],recall_list[i+1],precision_list[i+1],recall)
+
+def mean_precision1(results, relevant):
+    return (precision_at(0.25, results, relevant) +
+        precision_at(0.5, results, relevant) +
+        precision_at(0.75, results, relevant)) / 3
+
+def mean_precision2(results, relevant):
+
+    total=0
+
+    for i in range(10):
+        total+=precision_at((i+1)/10, results, relevant)
+
+
+    return (1/10)*total
+
+def norm_recall(results, relevant):
+    sum_rank=0
+    sum_i=0
+    for i in range(len(relevant)):
+        sum_i+=(i+1)
+        for j in range(len(results)):
+            if results[j]==relevant[i]:
+                sum_rank+=(j+1)
+    return 1-((sum_rank-sum_i)/(len(relevant)*(len(results)-len(relevant))))
+ 
+
+def norm_precision(results, relevant):
+    sum_rank=0
+    sum_i=0
+    for i in range(len(relevant)):
+        sum_i+=np.log(i+1)
+        for j in range(len(results)):
+            if results[j]==relevant[i]:
+                sum_rank+=np.log(j+1)
+
+    denominator=len(results)*np.log(len(results))-(len(results)-len(relevant))*np.log((len(results)-len(relevant)))-len(relevant)*np.log(len(relevant))
+
+    return 1- (sum_rank-sum_i)/denominator
+
+
+### Extensions
+
+# TODO: put any extensions here
+
+
+### Search
+
+def experiment():
+
+    docs = read_docs('cacm.raw')
+    queries = read_docs('query.raw')
+    rels = read_rels('query.rels')
+    stopwords = read_stopwords('common_words')
+
+    term_funcs = {
+        'tf': compute_tf,
+        'tfidf': compute_tfidf,
+        'boolean': compute_boolean
+    }
+
+    sim_funcs = {
+        'cosine': cosine_sim,
+        'jaccard': jaccard_sim,
+        'dice': dice_sim,
+        'overlap': overlap_sim
+    }
+
+    permutations = [
+        term_funcs,
+        [False, True],  # stem
+        [False, True],  # remove stopwords
+        sim_funcs,
+        [TermWeights(author=1, title=1, keyword=1, abstract=1),
+            TermWeights(author=1, title=3, keyword=4, abstract=1),
+            TermWeights(author=1, title=1, keyword=1, abstract=4)]
+    ]
+
+    print('term', 'stem', 'removestop', 'sim', 'termweights', 'p_0.25', 'p_0.5', 'p_0.75', 'p_1.0', 'p_mean1', 'p_mean2', 'r_norm', 'p_norm', sep='\t')
+
+    # This loop goes through all permutations. You might want to test with specific permutations first
+    for term, stem, removestop, sim, term_weights in itertools.product(*permutations):
+        processed_docs, processed_queries = process_docs_and_queries(docs, queries, stem, removestop, stopwords)
+        doc_freqs = compute_doc_freqs(processed_docs)
+        doc_vectors = [term_funcs[term](doc, doc_freqs, term_weights) for doc in processed_docs]
+
+        metrics = []
+
+        for query in processed_queries:
+            query_vec = term_funcs[term](query, doc_freqs, term_weights)
+            results = search(doc_vectors, query_vec, sim_funcs[sim])
+            # results = search_debug(processed_docs, query, rels[query.doc_id], doc_vectors, query_vec, sim_funcs[sim])
+            rel = rels[query.doc_id]
+
+            metrics.append([
+                precision_at(0.25, results, rel),
+                precision_at(0.5, results, rel),
+                precision_at(0.75, results, rel),
+                precision_at(1.0, results, rel),
+                mean_precision1(results, rel),
+                mean_precision2(results, rel),
+                norm_recall(results, rel),
+                norm_precision(results, rel)
+            ])
+
+        averages = [f'{np.mean([metric[i] for metric in metrics]):.4f}'
+            for i in range(len(metrics[0]))]
+        print(term, stem, removestop, sim, ','.join(map(str, term_weights)), *averages, sep='\t')
+
+
+
+def process_docs_and_queries(docs, queries, stem, removestop, stopwords):
+    processed_docs = docs
+    processed_queries = queries
+    if removestop:
+        processed_docs = remove_stopwords(processed_docs)
+        processed_queries = remove_stopwords(processed_queries)
+    if stem:
+        processed_docs = stem_docs(processed_docs)
+        processed_queries = stem_docs(processed_queries)
+    return processed_docs, processed_queries
+
+
+def search(doc_vectors, query_vec, sim):
+    results_with_score = [(doc_id + 1, sim(query_vec, doc_vec))
+                    for doc_id, doc_vec in enumerate(doc_vectors)]
+    results_with_score = sorted(results_with_score, key=lambda x: -x[1])
+    results = [x[0] for x in results_with_score]
+    return results
+
+
+def search_debug(docs, query, relevant, doc_vectors, query_vec, sim):
+    results_with_score = [(doc_id + 1, sim(query_vec, doc_vec))
+                    for doc_id, doc_vec in enumerate(doc_vectors)]
+    results_with_score = sorted(results_with_score, key=lambda x: -x[1])
+    results = [x[0] for x in results_with_score]
+
+    print('Query:', query)
+    print('Relevant docs: ', relevant)
+    print()
+    for doc_id, score in results_with_score[:10]:
+        print('Score:', score)
+        print(docs[doc_id - 1])
+        print()
+
+
+if __name__ == '__main__':
+    experiment()
